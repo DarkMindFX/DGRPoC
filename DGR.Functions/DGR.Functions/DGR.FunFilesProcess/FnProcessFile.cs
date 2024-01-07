b20af63e92fdcb4588cc74b72e1c2f3a3bd32771
DarkMindFX <eugene.ryzhov@gmail.com>
2024-01-07 16:47:55 +0100

    Impl. File processing function (triggers pipeline) + deployment

--- DGR.Functions/DGR.Functions/DGR.FunFilesProcess/FnProcessFile.cs
+++ DGR.Functions/DGR.Functions/DGR.FunFilesProcess/FnProcessFile.cs
@@ -1,9 +1,18 @@
 using System;
+using System.Collections.Generic;
 using System.Text.Json;
 using DGR.DTO;
+using DGR.Functions.Common;
+using Microsoft.Azure.Management.DataFactory;
+using Microsoft.Azure.Management.DataFactory.Models;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.OData.Edm;
+using Microsoft.Rest;
+using Newtonsoft.Json.Linq;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace DGR.FunFilesProcess
 {
@@ -12,13 +21,83 @@ namespace DGR.FunFilesProcess
         [FunctionName("FnProcessFile")]
         public void Run([QueueTrigger("dgr-files-queue", Connection = "AzureWebJobsStorage")] string message, ILogger log)
         {
-            log.LogInformation($"SplitPortfolio: {message}");
+            log.LogInformation($"FnProcessFile: {message}");
 
-            FileReadyMsg msgObject = JsonSerializer.Deserialize<DGR.DTO.FileReadyMsg>(message);
-            if (msgObject != null)
+            try
             {
 
+                FileReadyMsg msgObject = JsonSerializer.Deserialize<DGR.DTO.FileReadyMsg>(message);
+                if (msgObject != null)
+                {
+                    var funHelper = new FunctionHelper();
+
+                    var client = PrepareClient(funHelper);
+
+                    string resourceGroup = funHelper.GetEnvironmentVariable<string>(Constants.ENV_AZ_RESGROUP);
+                    string factoryName = funHelper.GetEnvironmentVariable<string>(Constants.ENV_AZ_FACTORY);
+                    string pipelineName = funHelper.GetEnvironmentVariable<string>(Constants.ENV_AZ_PIPELINE);
+
+                    // ****** Run pipeline *************
+                    CreateRunResponse runResponse;
+                    PipelineRun pipelineRun;
+
+                    log.LogInformation("Called pipeline with parameters.");
+
+                    // preparing parameters
+                    Dictionary<string, object> parameters = new Dictionary<string, object>();
+                    parameters["file_path"] = msgObject.FilePath;
+                    parameters["business_date"] = msgObject.BusinessDate.ToString();
+                    parameters["portfolio_id"] = msgObject.PortfolioID.ToString();
+
+                    // Running pipeline
+                    runResponse = client.Pipelines.CreateRunWithHttpMessagesAsync(
+                        resourceGroup, factoryName, pipelineName, parameters: parameters).Result.Body;
+
+                    log.LogInformation("Pipeline run ID: " + runResponse.RunId);
+
+                    //Wait and check for pipeline result
+                    log.LogInformation("Checking pipeline run status...");
+                    bool isRunning = true;
+                    while (isRunning)
+                    {
+                        pipelineRun = client.PipelineRuns.Get(
+                            resourceGroup, factoryName, runResponse.RunId);
+
+                        log.LogInformation("Status: " + pipelineRun.Status);
+
+                        if (pipelineRun.Status != "InProgress" && pipelineRun.Status != "Queued")
+                            isRunning = false;
+                        else
+                            System.Threading.Thread.Sleep(15000);
+
+                    }
+                }
             }
+            catch(Exception ex)
+            {
+                log.LogError($"{ex.Message}\r\nStackTrace: {ex.StackTrace}");
+            }
+        }
+
+        private DataFactoryManagementClient PrepareClient(FunctionHelper funHelper)
+        {
+            string tenantId = funHelper.GetEnvironmentVariable<string>(Constants.ENV_AZ_TENANTID);
+            string applicationId = funHelper.GetEnvironmentVariable<string>(Constants.ENV_AZ_APPID);
+            string authenticationKey = funHelper.GetEnvironmentVariable<string>(Constants.ENV_AZ_AUTHKEY);
+            string subscriptionId = funHelper.GetEnvironmentVariable<string>(Constants.ENV_AZ_SUBSCRIPTIONID);
+
+            //Create a data factory management client
+            var context = new AuthenticationContext("https://login.windows.net/" + tenantId);
+            ClientCredential cc = new ClientCredential(applicationId, authenticationKey);
+            AuthenticationResult result = context.AcquireTokenAsync("https://management.azure.com/", cc).Result;
+            ServiceClientCredentials cred = new TokenCredentials(result.AccessToken);
+            var client = new DataFactoryManagementClient(cred)
+            {
+                SubscriptionId = subscriptionId
+            };
+
+            return client;
+
         }
     }
 }
--- DGR.Functions/DGR.Functions/DGR.Functions.Common/Constants.cs
+++ DGR.Functions/DGR.Functions/DGR.Functions.Common/Constants.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +14,12 @@ namespace DGR.Functions.Common
         public static string ENV_Q_PORTFOLIOS = "Q_Portfolios";
         public static string ENV_Q_FILESINFO = "Q_Files";
         public static string ENV_DATA_CHUNK_SIZE = "Data_Chunk_Size";
+        public static string ENV_AZ_TENANTID = "AZ_TenantID";
+        public static string ENV_AZ_APPID = "AZ_AppID";
+        public static string ENV_AZ_AUTHKEY = "AZ_AuthKey";
+        public static string ENV_AZ_SUBSCRIPTIONID = "AZ_SubscriptionID";
+        public static string ENV_AZ_RESGROUP = "AZ_ResourceGroup";
+        public static string ENV_AZ_FACTORY = "AZ_FactoryName";
+        public static string ENV_AZ_PIPELINE = "AZ_PipelineName";
     }
 }
